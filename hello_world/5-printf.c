52322ef4177ee1efc82db89ce2d01d60a9cbe144
AmarceQ <marcequintero016@gmail.com>
2022-10-09 15:17:28 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* int main - C program that prints
+* @a: Marcela Quintero
+*
+* Description: C program that prints ...
+* Use the function printf
+*/
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
