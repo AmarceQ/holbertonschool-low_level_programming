78545d7c80ea7b81f2390e0923c200bab953123c
AmarceQ <marcequintero016@gmail.com>
2022-10-09 14:52:47 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#!/bin/bash
-
 #include <stdio.h>
 /**
 * struct my_struct - Write a C program
-*
 * @a: Marcela Quintero
-*
 * Description: C program  that prints exactly ...
 * Use the function puts
 */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle");
-			return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
