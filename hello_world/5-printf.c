4e4b60372851057d9a35b999c8280da3f28b867a
AmarceQ <marcequintero016@gmail.com>
2022-10-09 15:56:14 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 /**
-* @a: Marcela Quintero
-* Description: C program that prints with 
-* Use the function printf
-* main - Starting point for program execution
-* Return: Always 0 (Succes)
+ * main - Starting point for program execution
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
